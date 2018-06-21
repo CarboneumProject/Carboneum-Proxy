347dca7c967ee655fee0bef7a0082aadcc7e70a0
Pisut Trimahawong <pisut@siamsquared.com>
2018-06-21 18:41:44 +0700

    fixed getval

--- routes/getval.js
+++ routes/getval.js
@@ -2,22 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var getval = require("../model/getval");
+var exchange = require("../model/exchange");
 
 
 var ExchangeError = require("../model/exchangeerror");
 
-/* GET home page. */
+router.post('/', function(req, res, next) {
+    if (exchange[req.query.exchange]) {
+        getval.key(req, res, next);
+    } else {
+        return next(new ExchangeError('Exchange not found!', 9000));
+    }
+});
 
-module.exports = function (exchange) {
-    /* GET home page. */
-    router.post('/', function(req, res, next) {
-        console.log('rub');
-        if (exchange[req.query.exchange]) {
-            getval.key(req, res, next);
-        } else {
-            return next(new ExchangeError('Exchange not found!', 9000));
-        }
-    });
-
-    return router;
-};
+module.exports = router;
