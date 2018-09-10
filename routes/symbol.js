1f44829fedf397bb7766b879ef71f55d5ca3ca2d
chonnimit eksompanthapap <chonimit@siamsquared.com>
2018-09-10 22:22:59 +0700

    get symbol by exchange

--- routes/symbol.js
+++ routes/symbol.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const ExchangeError = require("../model/exchangeError");
 
 const router = express.Router();
 
-router.get('/list', async (req, res) => {
+router.get('/list', async (req, res, next) => {
   const symbol = await require('../model/symbol');
-  res.send(Object.keys(symbol['carboneum']));
+
+  if (req.query.exchange) {
+    if (symbol[req.query.exchange]) {
+      let result = [];
+
+      for (let k in symbol[req.query.exchange]) {
+        if (symbol[req.query.exchange].hasOwnProperty(k)) {
+          result.push(symbol[req.query.exchange][k]);
+        }
+      }
+
+      res.send(result);
+    } else {
+      next(new ExchangeError('Exchange not found!', 9000));
+    }
+  } else {
+    res.send(Object.keys(symbol['carboneum']));
+  }
 });
 
 module.exports = router;
